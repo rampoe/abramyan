7ca91495581397f979b13590d11a1aa7a1342b0d
Rahman Begjanov <mail.begman@gmail.com>
2024-01-22 15:33:09 +0500

    sync

---
+++ begin31.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long double tf,tc;
+	cin>>tc;
+	tf=(9*tc+160)/5;
+	cout<<tf;
+	return 0;
+}
---
+++ begin32.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long double tf;
+cin>>tf;
+cout<<(tf-32)*5/9;
+return 0;}
