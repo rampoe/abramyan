21f27e2b6214d8b289944b567cbe91cc5c0bddaa
Rahman Begjanov <mail.begman@gmail.com>
2024-01-26 16:12:22 +0500

    sync

---
+++ integer15.cpp
@@ -0,0 +1,4 @@
+#include<bits/stdc++.h>
+using namespace std;int main(){
+	int n,n1,n2;cin>>n;cout<<n%100/10<<n/100<<n%10;
+}
---
+++ integer16.cpp
@@ -0,0 +1,4 @@
+#include<bits/stdc++.h>
+using namespace std;int main(){
+	int n;cin>>n;cout<<n/100<<n%10<<n%100/10;
+}
