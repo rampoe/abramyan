a3b185b545e437c5e93237c2c4cc4db3dcec608f
rampoe <mail.rampoe@gmail.com>
2024-02-04 18:58:50 +0500

    sync

--- integer30.cpp
+++ integer30.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-int main() {
-	unsigned long long n = 0;
-	std::cout << "Enter the year: ";
-	std::cin >> n;
-	std::cout << n / 100;
-	return 0;
+int main()
+{
+  unsigned long long n = 0;
+  std::cout << "Enter the year: ";
+  std::cin >> n;
+  std::cout << n / 100 + 1 << "th century" << std::endl;
+  return 0;
 }
