a5fb8f8bf616f5fe4e4f7ff5095adff07fb65be0
rampoe <mail.rampoe@gmail.com>
2024-02-04 19:58:58 +0500

    sync

---
+++ boolean/boolean7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+  long long a = 0, b = 0, c = 0;
+  std::cout << "Enter A, B and C: ";
+  std::cin >> a >> b >> c;
+
+  if (a < b && b < c || c < b && b < a)
+  {
+    std::cout << true;
+  }
+
+  else
+  {
+    std::cout << false;
+  }
+
+  return 0;
+}
