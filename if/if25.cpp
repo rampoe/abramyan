9017cc0e53380b21e2eb0b0a9994e6cbd1f99db4
rampoe <mail.rampoe@gmail.com>
2024-02-28 16:31:23 +0500

    sync

---
+++ if/if25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  float x = 0;
+
+  std::cin >> x;
+
+  std::cout << "y = ";
+  if (x < -2 && x > 2)
+  {
+    std::cout << 2 * x << std::endl;
+  }
+  else
+  {
+    std::cout << -3 * x << std::endl;
+  }
+
+  return 0;
+}
