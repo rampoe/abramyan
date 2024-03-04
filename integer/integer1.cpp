e8f9e33b96e283c89186126ba88eaab8970ea824
rampoe <mail.rampoe@gmail.com>
2024-03-04 17:50:59 +0500

    sync

--- integer/integer1.cpp
+++ integer/integer1.cpp
@@ -1,6 +1,8 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
-	int l;cin>>l;
-	cout<<l<<"cm="<<l/100<<"m";
+int main()
+{
+  int l;
+  cin >> l;
+  cout << l << "cm=" << l / 100 << "m";
 }
