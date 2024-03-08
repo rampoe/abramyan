3382d91f9ff1fd9a3ed1c991e669af11964cbc1e
rampoe <mail.rampoe@gmail.com>
2024-03-08 13:25:02 +0500

    sync

--- case/case19.cpp
+++ case/case19.cpp
@@ -4,26 +4,26 @@ using namespace std;
 int main()
 {
 	int n, o; cin >> n;
-	o = n % 60;
+	o = (n + 56) % 60;
 	cout << "god";
-	if (o >= 0 && o < 12) cout << " chernoy";
-	else if (o >= 12 && o < 24) cout << " zelyonoy";
-	else if (o >= 24 && o < 36) cout << " krasnoy";
-	else if (o >= 36 && o < 48) cout << " zheltoy";
-	else cout << " beloy";
-	switch(n % 12)
+	if (o >= 0 && o < 12) cout << " zelyon";
+	else if (o >= 12 && o < 24) cout << " krasn";
+	else if (o >= 24 && o < 36) cout << " zhelt";
+	else if (o >= 36 && o < 48) cout << " bel";
+	else cout << " chern";
+	switch((n + 8) % 12)
 	{
-	case 0:  cout << " kurisy";   break;
-	case 1:  cout << " sobaki";   break;
-	case 2:  cout << " svinyi";   break;
-	case 3:  cout << " krysy";    break;
-	case 4:  cout << " korovy";   break;
-	case 5:  cout << " tigra";    break;
-	case 6:  cout << " zaytsa";   break;
-	case 7:  cout << " drakona";  break;
-	case 8:  cout << " zmei";     break;
-	case 9:  cout << " loshadi";  break;
-	case 10: cout << " ovtsy";    break;
-	case 11: cout << " obezyany"; break;
+	case 0:  cout << "oy krysy"; break;
+	case 1:  cout << "oy korovy"; break;
+	case 2:  cout << "ogo tigra"; break;
+	case 3:  cout << "ogo zaytsa"; break;
+	case 4:  cout << "ogo drakona"; break;
+	case 5:  cout << "oy zmei"; break;
+	case 6:  cout << "oy loshadi"; break;
+	case 7:  cout << "oy ovtsy"; break;
+	case 8:  cout << "oy obezyany"; break;
+	case 9:  cout << "oy kuritsy"; break;
+	case 10: cout << "oy sobaki"; break;
+	case 11: cout << "oy svinyi"; break;
 	}
 }
