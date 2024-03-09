c1b3652c6538749ed129f3185ced01a98c2a8793
rampoe <mail.rampoe@gmail.com>
2024-03-09 12:01:36 +0500

    sync

---
+++ for/for20.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    float cs = 1, s = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cs *= i;
+        s += cs;
+    }
+    cout << s;
+    return 0;
+}
