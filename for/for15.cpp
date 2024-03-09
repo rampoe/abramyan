b12d3d2196689eff466f298999e0f411d794ecff
rampoe <mail.rampoe@gmail.com>
2024-03-09 10:53:41 +0500

    sync

---
+++ for/for15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, p = 1;
+    int n;
+    cin >> a >> n;
+    for (int i = 0; i < n; i++)
+    {
+        p *= a;
+    }
+    cout << p;
+    return 0;
+}
