54f5a26573562085faac55cbc8e1844ac4e9d9e8
rampoe <mail.rampoe@gmail.com>
2024-03-09 10:49:49 +0500

    sync

--- for/for14.cpp
+++ for/for14.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main()
 {
-    int n; cin >> n;
-    
+    int n, s = 0; cin >> n;
+    for (int i = 1; i <= 2 * n - 1; i += 2)
+    {
+        s += i;
+        cout << s << endl;
+    }
     return 0;
 }
