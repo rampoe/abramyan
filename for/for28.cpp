a09c6f2e7d9513795169e9b03480c13fa7cd8468
rampoe <mail.rampoe@gmail.com>
2024-03-10 22:49:58 +0500

    sync

--- for/for28.cpp
+++ for/for28.cpp
@@ -3,38 +3,30 @@
 
 int main()
 {
-    long double x = 0;
     std::cout << "X = ";
+    long double x = 0;
     std::cin >> x;
-
-    long long int n = 0;
     std::cout << "N = ";
+    unsigned long long int n = 0;
     std::cin >> n;
-
-    long double u = 1;
-    long double d = 1;
-
-    long double s = 1 + x / 2;
-    for (long long int i = 2; i <= n; i++)
+    long double output_sum = 1 + x / 2;
+    for (unsigned long long int i = 2; i <= n; i++)
     {
-        long long int sign = std::pow(-1, i - 1);
-
-        for (long long int j = 1; j <= 2 * i - 3; j += 2)
+        long long int sign = std::pow(-1, n - 1);
+        long double up = 1;
+        for (unsigned long long int j = 1; j <= 2 * i - 3; j += 2)
         {
-            u *= j;
+            up *= j;
         }
-        u *= std::pow(x, i);
-
-        for (long long int j = 1; j <= 2 * i; j += 2)
+        up *= std::pow(x, i);
+        long double down = 1;
+        for (unsigned long long int j = 2; j <= 2 * i; j += 2)
         {
-            d *= j;
+            down *= j;
         }
-
-        s += sign * u / d;
+        output_sum += sign * up / down;
     }
-
-    std::cout << "Answer: " << s << std::endl;
+    std::cout << "Output: " << output_sum << std::endl;
     std::cout << "sqrt(1 + x) = " << std::sqrt(1 + x) << std::endl;
-
     return 0;
 }
