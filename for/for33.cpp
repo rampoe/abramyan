ba0ba6a95565a5eef22e7c85e75909df01f4a4c4
rampoe <mail.rampoe@gmail.com>
2024-03-10 23:57:44 +0500

    sync

--- for/for33.cpp
+++ for/for33.cpp
@@ -6,7 +6,17 @@ int main()
     std::cout << "N = ";
     unsigned long long int n = 0;
     std::cin >> n;
-    
+    unsigned long long int f1 = 1;
+    unsigned long long int f2 = 1;
+    std::cout << 1 << " " << 1 << " ";
+    for (unsigned long long int i = 3; i <= n; i++)
+    {
+        unsigned long long int temp = f2;
+        f2 = f1 + f2;
+        f1 = temp;
+        std::cout << f2 << " ";
+    }
+    std::cout << std::endl;
     std::system("pause");
     return 0;
 }
