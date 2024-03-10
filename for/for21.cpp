862e17537ebb0b63610e775ac4d0fa28021eb1c5
rampoe <mail.rampoe@gmail.com>
2024-03-10 17:39:08 +0500

    sync

--- for/for21.cpp
+++ for/for21.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
+
+unsigned long long int factorial(unsigned long long int n)
+{
+    if (n == 0 || n == 1) return 1;
+    unsigned long long int result = 1;
+    for (unsigned long long int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    float s = 1 , cs = 1;
-    for (int i = 1; i <= n; i++)
+    unsigned long long int n = 0;
+    std::cout << "N = ";
+    std::cin >> n;
+    long double s = 0;
+    for (unsigned long long int i = 0; i <= n; i++)
     {
-        cs *= (float)i;
-        s += 1/(float)cs;
+        s += 1.0 / factorial(i);
     }
-    cout << s;
+    std::cout << "Output: " << s << std::endl;
+    std::cout << "e = exp(1) = " << std::exp(1) << std::endl;
     return 0;
 }
