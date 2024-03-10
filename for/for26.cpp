5e6b42f088ef8a4e96e8b7078d4b98f2cec9f40e
rampoe <mail.rampoe@gmail.com>
2024-03-10 21:35:19 +0500

    sync

--- for/for26.cpp
+++ for/for26.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
-int main() {
-    float x, s = 0;
-    int n;
-    cin >> x >> n;
-    for (int i = 1; i <= n; i++) {
-        s += pow(-1, i) * pow(x, 2 * i + 1) / (2 * i + 1);
+int main()
+{
+    long double x = 0;
+    std::cout << "X = ";
+    std::cin >> x;
+    unsigned long long int n = 0;
+    std::cout << "N = ";
+    std::cin >> n;
+    long double output_sum = 0;
+    for (unsigned long long int i = 0; i <= n; i++)
+    {
+        output_sum += std::pow(-1, i) * std::pow(x, 2 * i +  1) / (2 * i + 1);
     }
-    cout << s << endl;
+    std::cout << "Output: " << output_sum << std::endl;
+    std::cout << "arctg(x) = " << std::atan(x) << std::endl;
     return 0;
 }
