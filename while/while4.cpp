f015ce6370db351e32cec8acd8df8dbf3f98527c
rampoe <mail.rampoe@gmail.com>
2024-03-11 10:42:52 +0500

    sync

---
+++ while/while4.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    std::cout << "N: ";
+    unsigned long long int n = 0;
+    std::cin >> n;
+    bool flag = false;
+    unsigned long long int current_number = 3;
+    while (current_number <= n)
+    {
+        if (current_number == n)
+        {
+            flag = true;
+        }
+        current_number *= 3;
+    }
+    std::cout << "Output: " << flag << std::endl;
+    std::system("pause");
+    return 0;
+}
