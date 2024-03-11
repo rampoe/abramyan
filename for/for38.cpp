9b8bb0476d91f077a4d2baf5c22a5f8c7ed204b0
rampoe <mail.rampoe@gmail.com>
2024-03-11 10:04:23 +0500

    sync

---
+++ for/for38.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << "N = ";
+    unsigned long long int n = 0;
+    std::cin >> n;
+    long double output_sum = 0;
+    unsigned long long int counter = 1;
+    for (unsigned long long int i = n; i >= 1; i--)
+    {
+        output_sum += std::pow(counter, i);
+    }
+    std::cout << "Output: " << output_sum << std::endl;
+    std::system("pause");
+    return 0;
+}
